3e2178d0bd7cce21ef2f43bc3347b5653e36d7a8
Lint Action <lint-action@samuelmeuli.com>
2021-05-03 17:00:56 +0000

    Fix code style issues with ESLint

--- auth/clients.js
+++ auth/clients.js
@@ -136,7 +136,7 @@ module.exports = [
 					console.log(req.body);
 					if (req.body.code && req.body.email) {
 						const { email = null, date = null } =
-							(await database.get(`EMAIL_${req.body.code}`)) ||
+							await database.get(`EMAIL_${req.body.code}`) ||
 							{};
 						if (Date.now() - date > 900000) {
 							await database.delete(`EMAIL_${req.body.code}`);
