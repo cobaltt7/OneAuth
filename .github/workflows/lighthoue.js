0225a8c714cb80e0d68536058e81723e237ad84b
RedGuy12 <paul@reid-family.org>
2021-05-06 09:40:46 -0500

    workflow should be done

---
+++ .github/workflows/lighthoue.js
@@ -0,0 +1,23 @@
+"use strict";
+
+const github = require("@actions/github");
+
+const octokit = github.getOctokit(process.argv[2]);
+
+const { data } = JSON.parse(process.argv[3]);
+
+let OUTPUT =
+	`# This week's Lighthouse scores\n` +
+	`URL|Accessibility|Best Practices|Performace|Progressive Web App|SEO\n` +
+	`-|-|-|-|-|-\n`;
+
+data.forEach((result) => {
+	OUTPUT += `${result.url}|${Object.values(result.scores).join("|")}\n`;
+});
+
+console.log(data);
+octokit.issues.createComment({
+	...github.context.repo,
+	issue_number: "27",
+	body: OUTPUT,
+});
