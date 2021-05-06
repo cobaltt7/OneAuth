3f526535902dd777a74b063c744d220ef9d95433
RedGuy12 <paul@reid-family.org>
2021-05-06 11:36:43 -0500

    more info

--- .github/workflows/lighthouse.js
+++ .github/workflows/lighthouse.js
@@ -1,25 +1,32 @@
 "use strict";
 
 const github = require("@actions/github");
-
 const octokit = github.getOctokit(process.argv[2]);
 
-console.log(process.argv[3])
-const {data} = JSON.parse(process.argv[3]);
-
+const { data } = JSON.parse(process.argv[3]);
 
 let OUTPUT =
 	`# This week's Lighthouse scores\n` +
-	`URL|Accessibility|Best Practices|Performace|Progressive Web App|SEO\n` +
-	`-|-|-|-|-|-\n`;
+	`URL|Device|Accessibility|Best Practices|Performace|Progressive Web App|SEO|More Info\n` +
+	`-|-|-|-|-|-|-\n`;
 
-data.forEach((result) => {
-	OUTPUT += `${result.url}|${Object.values(result.scores).join("|")}\n`;
+data.forEach(
+	(result) =>
+		(OUTPUT += `${result.url}|${result.emulatedFormFactor}|${Object.values(result.scores).join(
+			"|",
+		)}|https://developers.google.com/speed/pagespeed/insights/?url=${encodeURIComponent(
+			result.url,
+		)}&tab=${result.emulatedFormFactor}\n`),
+);
+
+octokit.issues.create({
+	...github.context.repo,
+	title: "Lighthouse Performance Updates",
+	body: "I will post Lighthouse performance updates here weekly!",
 });
 
-console.log(data);
 octokit.issues.createComment({
 	...github.context.repo,
-	issue_number: "27",
+	issue_number: "29",
 	body: OUTPUT,
 });
