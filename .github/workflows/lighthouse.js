18f144acdf02c998230c749a28437809470f5e55
RedGuy12 <paul@reid-family.org>
2021-05-06 11:47:50 -0500

    fix: dup issues, tabes

--- .github/workflows/lighthouse.js
+++ .github/workflows/lighthouse.js
@@ -7,23 +7,17 @@ const { data } = JSON.parse(process.argv[3]);
 
 let OUTPUT =
 	`# This week's Lighthouse scores\n` +
-	`URL|Device|Accessibility|Best Practices|Performace|Progressive Web App|SEO|More Info\n` +
-	`-|-|-|-|-|-|-\n`;
+	`URL | Device | Accessibility | Best Practices | Performace | Progressive Web App | SEO | More Info\n` +
+	`- | - | - | - | - | - | -\n`;
 
 data.forEach(
 	(result) =>
-		(OUTPUT += `${result.url}|${result.emulatedFormFactor}|${Object.values(result.scores).join(
-			"|",
-		)}|https://developers.google.com/speed/pagespeed/insights/?url=${encodeURIComponent(
+		(OUTPUT += `${result.url} | ${result.emulatedFormFactor} | ${Object.values(result.scores).join(
+			" | ",
+		)} | https://developers.google.com/speed/pagespeed/insights/?url=${encodeURIComponent(
 			result.url,
 		)}&tab=${result.emulatedFormFactor}\n`),
 );
-
-octokit.issues.create({
-	...github.context.repo,
-	title: "Lighthouse Performance Updates",
-	body: "I will post Lighthouse performance updates here weekly!",
-});
 
 octokit.issues.createComment({
 	...github.context.repo,
