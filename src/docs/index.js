23baf3c32ee6dc01707cc9ebd3e4fcfdfa83f064
RedGuy7 <>
2021-05-12 21:38:12 +0000

    code unreadable on docs bugfix

--- src/docs/index.js
+++ src/docs/index.js
@@ -34,7 +34,7 @@ router.use((req, res, next) => {
 	if (fileSystem.existsSync(filename)) {
 		const markdown = fileSystem.readFileSync(filename, "utf8");
 		return res.render(path.resolve(__dirname, "markdown.html"), {
-			content: marked(markdown),
+			content: marked(markdown).replace(/<pre><code>/g,'<pre class="hljs"><code>'),
 			title: /^#\s(?<heading>.+)$/m.exec(markdown).groups.heading,
 		});
 	}
