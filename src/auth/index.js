f5b8b9062e97bcf109f3d888e27a8849bdfc9059
wgytbot <83586655+wgytbot@users.noreply.github.com>
2021-06-07 23:33:21 +0000

    Lint and format code
    Signed-off-by: wgytbot <83586655+wgytbot@users.noreply.github.com>

--- src/auth/index.js
+++ src/auth/index.js
@@ -174,7 +174,8 @@ for (const method of [
 				res,
 				/** .........
 				 * ......... ......... ......... ......... ......... ......... ......... .........
-				 * Passes information from the authentication handler (and other sources) to sendResponse.
+				 * ......... Passes information from the authentication handler (and other sources)
+				 * to sendResponse.
 				 *
 				 * @param {import("../../types").sendResponseArgs} args - Information from the
 				 *   authentication handler.
@@ -245,7 +246,8 @@ router.get(
 router.get(
 	"/backend/send_data",
 	/** .........
-	 * ......... ......... ......... ......... ......... ......... ......... ......... Save the user's data.
+	 * ......... ......... ......... ......... ......... ......... ......... ......... .........
+	 * Save the user's data.
 	 *
 	 * @param {import("../../types").ExpressRequest} req - Express request object.
 	 * @param {import("../../types").ExpressResponse} res - Express response object.
