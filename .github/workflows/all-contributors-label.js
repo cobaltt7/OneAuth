fc328869c8ef80afd9b64740e6a5e980db9b72a0
RedGuy12 <paul@reid-family.org>
2021-08-17 16:00:27 -0500

    lint

--- .github/workflows/all-contributors-label.js
+++ .github/workflows/all-contributors-label.js
@@ -1,45 +1,73 @@
 /**
- * @file Workflow To add commiters to the credits page when their issue is process.argv[2]ed for the related
+ * @file Workflow To add commiters to the credits page when their issue is labeled for the related
  *   contribution types.
  */
 
- import fileSystem from "fs";
- import path from "path";
+import fileSystem from "fs";
+import path from "path";
 import url from "url";
 
-const
- username = process.argv[3] || process.argv[4] || process.argv[5],
-/** @type {import("../../types").AllContributosRc} */ config = JSON.parse(
+const username = process.argv[3] || process.argv[4] || process.argv[5];
+/** @type {import("../../types").AllContributosRc} */
+// eslint-disable-next-line one-var -- `contributions` depends on `username`.
+const config = JSON.parse(
 		fileSystem.readFileSync(url.pathToFileURL(path.resolve(".all-contributorsrc")), "utf8"),
-		),contributions = config.contributors?.find(({ login }) => login === username)?.contributions || [];
+	),
+	contributions =
+		config.contributors?.find(({ login }) => login === username)?.contributions || [];
 
-if (process.argv[2] === "scope: a11y") contributions.push("a11y");
+switch (process.argv[2]) {
+	case "scope: a11y": {
+		contributions.push("a11y");
 
-else if (process.argv[2] === "type: security") contributions.push("security");
+		break;
+	}
 
-else if (process.argv[2] === "scope: i18n") contributions.push("translation");
+	case "type: security": {
+		contributions.push("security");
 
-else if (process.argv[2] === "type: announcement" || process.argv[2] === "scope: dependencies")
-	contributions.push("maintenance");
+		break;
+	}
 
-else if (process.argv[2].startsWith("type: bug")) contributions.push("bug");
+	case "scope: i18n": {
+		contributions.push("translation");
 
-else if (process.env.GITHUB_EVENT_NAME === "pull_request") {
-	if (process.argv[2] === "scope: documentation")
-		contributions.push("doc");
+		break;
+	}
 
-	else if (process.argv[2] === "scope: meta") contributions.push("infra");
+	case "type: announcement":
+	case "scope: dependencies": {
+		contributions.push("maintenance");
+	}
+}
 
-	else if (process.argv[2] === "language: sass" || process.argv[2] === "scope: design")
-		contributions.push("design");
+if (process.argv[2].startsWith("type: bug")) {
+	contributions.push("bug");
+} else if (process.env.GITHUB_EVENT_NAME === "pull_request") {
+	switch (process.argv[2]) {
+		case "scope: documentation": {
+			contributions.push("doc");
+
+			break;
+		}
+
+		case "scope: meta": {
+			contributions.push("infra");
+
+			break;
+		}
+
+		case "language: sass":
+		case "scope: design": {
+			contributions.push("design");
+
+			break;
+		}
+	}
 } else if (process.argv[2].startsWith("type: enhancement")) {
 	contributions.push("ideas");
 }
 
-console.log(
-	`npx all-contributors-cli add ${username} "${contributions.join(
-		",",
-	)}"`,
-)
+console.log(`npx all-contributors-cli add ${username} "${contributions.join(",")}"`);
 
 export default undefined;
